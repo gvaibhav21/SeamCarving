32b13da73188c690146cb7ec2a0c43c02cc0426f
Soumya <agr.soumya@gmail.com>
2017-03-15 08:25:11 +0530

    upscaling implemented vertical seam okay, horizontal seam bug

--- seamcarvinglib/downscale.cpp
+++ seamcarvinglib/downscale.cpp
@@ -143,6 +143,7 @@ public:
 		if(pixelarray[id].left !=-1)	{ cnt++; Sum += getDiff(id,pixelarray[id].left); }
 		return pixelarray[id].energy = Sum/cnt;
 	}
+
 	
 };
 
@@ -152,6 +153,7 @@ int pos[1010];
 
 int getptr(graph &g, int x,int y)
 {
+	cout<<"here!"<<endl;
 	int cur = g.topleft;
 	while(x--)
 		cur = g.pixelarray[cur].bottom;
@@ -161,8 +163,7 @@ int getptr(graph &g, int x,int y)
 }
 
 graph remove_v_seam(graph &g)
-{
-	int m=g.width,n=g.height,i,j;
+{int m=g.width,n=g.height,i,j;
 
 	int cur = g.topleft, rowstart = g.topleft;
 	
@@ -431,20 +432,425 @@ void downscale(const Mat &image, double r_height, double r_width)
 {
 	downscale(image, (int)(image.size().height*(1 - r_height)), (int)(image.size().width*(1 - r_width)));
 }
+graph insert_h_seam(graph &g)
+{
+	cout<<"inserting horiz seam!"<<endl;
+	int m=g.width,n=g.height,i,j;
+
+	int cur = g.topleft, rowstart = g.topleft;
+	
+	for(i=0;i<n;i++)
+	{
+		cur = rowstart;
+		for(j=0;j<m;j++)
+		{
+						cout<<"nextcur: "<<cur;
+
+			pixel_energy[i][j] = g.getEnergy(cur);
+			cur = g.pixelarray[cur].right;
+		}
+		rowstart = g.pixelarray[rowstart].bottom;
+	}
+	cout<<"calculating dp "<<endl;
+	for(i=0;i<n;i++)
+		inner_dp[i][0] = pixel_energy[i][0];
+	for(j=1;j<m;j++)
+	{
+		for(i=0;i<n;i++)
+		{
+			double e = pixel_energy[i][j];
+			inner_dp[i][j] = e + inner_dp[i][j-1];
+			inner_choice[i][j] = i;
+			if(i && inner_dp[i][j] > e + inner_dp[i-1][j-1])
+			{
+				inner_dp[i][j] = e + inner_dp[i-1][j-1];
+				inner_choice[i][j] = i-1;
+			}
+			if(i+1 < n && inner_dp[i][j] > e + inner_dp[i+1][j-1])
+			{
+				inner_dp[i][j] = e + inner_dp[i+1][j-1];
+				inner_choice[i][j] = i+1;
+			}
+		}
+	}
+	double minn = 1e50;
+	int minpos;
+	for(i=0;i<n;i++)
+	{
+		if(inner_dp[i][m-1] < minn)
+		{
+			minn = inner_dp[i][m-1];
+			minpos = i;
+		}
+	}
+
+	cur = minpos;
+	pos[m-1] = minpos;
+	for(i=m-2;i>=0;--i)
+	{
+		cur = inner_choice[cur][i+1];
+		pos[i] = cur;
+	}
+
+	graph New;
+	New = g;
+	New.height = New.height + 1;
+	cout<<"I am here!"<<endl;
+	cur = getptr(New,pos[m-1],m-1);
+
+	int index = New.pixelarray_size;
+	New.pixelarray.push_back(pixel(New.pixelarray[cur].val[0],New.pixelarray[cur].val[1],New.pixelarray[cur].val[2] ));
+	New.pixelarray_size ++;
+	int Top = New.pixelarray[cur].top, Bottom = New.pixelarray[cur].bottom;
+	cout<<"top="<<Top<<" bottom= "<<Bottom<<"\n";
+	for(i=m-1;i>=0;--i)
+	{
+			// cout<<"cur: "<<cur<<endl;
+			// cout<<"index: "<<index<<" matrix last index: "<<New.pixelarray.size()-1;
+			 cout<<"here1"<<endl;
+
+			New.pixelarray[cur].bottom = index;
+			New.pixelarray[cur].updated = false;
+		
+		if(Bottom!=-1)
+		{
+			New.pixelarray[Bottom].top = index;
+			New.pixelarray[Bottom].updated = false;
+		}
+		New.pixelarray[index].top = Top;
+		New.pixelarray[index].bottom = Bottom;
+
+
+
+		if(!i)
+		{
+			if(cur == New.topleft)
+			{
+				// cout<<"true!"<<endl;
+
+				// New.topleft = New.pixelarray[cur].bottom;
+			}
+			break;
+		}
+		if(pos[i]==pos[i-1]){
+						 cout<<"here2"<<endl;
+
+			int prevcur = cur;
+			cur = New.pixelarray[cur].left;
+			//make new pixel & set the corresponding right for it
+			Top = New.pixelarray[cur].top; Bottom = New.pixelarray[cur].bottom;
+			New.pixelarray.push_back(pixel(New.pixelarray[cur].val[0],New.pixelarray[cur].val[1],New.pixelarray[cur].val[2]));
+			New.pixelarray_size ++;
+			
+			New.pixelarray[index].left = index+1;
+			New.pixelarray[index+1].right = index;
+			index++;
+		}
+		else if(pos[i]>pos[i-1])
+		{
+
+									 cout<<"here3"<<endl;
+									 try{
+			int prevcur = cur;
+						cout<<"oldcur"<<cur<<endl;
+
+			int nextleft = New.pixelarray[cur].left;
+			cout<<"nextleft"<<nextleft<<endl;
+			cur = New.pixelarray[New.pixelarray[cur].left].top;
+			cout<<"i: "<<i<<endl;
+			cout<<"posi"<<pos[i]<<"posi-1"<<pos[i-1]<<endl;
+			cout<<"newcur"<<cur<<endl;
+			cout<<"top"<<Top<<"Bottom"<<Bottom;
+			Top = New.pixelarray[cur].top; Bottom = New.pixelarray[cur].bottom;
+			New.pixelarray.push_back(pixel(New.pixelarray[cur].val[0],New.pixelarray[cur].val[1],New.pixelarray[cur].val[2]));
+			New.pixelarray[prevcur].left=index+1;
+			New.pixelarray[index+1].right = prevcur;
+			New.pixelarray_size ++;
+			New.pixelarray[index].left = nextleft;
+
+			New.pixelarray[nextleft].right = index;
+			index++;
+
+		}
+		catch(Exception e)
+		{
+			cout<<"exception"<<e.what()<<endl;
+		}
+
+		}
+		else
+		{
+			 cout<<"here4"<<endl;
+			cur = New.pixelarray[New.pixelarray[cur].left].bottom;	
+			int nextleft = cur;
+			int prevbot = Bottom;
+			Top = New.pixelarray[cur].top; Bottom = New.pixelarray[cur].bottom;
+			New.pixelarray.push_back(pixel(New.pixelarray[cur].val[0],New.pixelarray[cur].val[1],New.pixelarray[cur].val[2]));
+			New.pixelarray_size ++;
+			New.pixelarray[index+1].right = prevbot;
+			New.pixelarray[prevbot].left = index+1;
+			New.pixelarray[index].left = nextleft;
+			New.pixelarray[nextleft].right = index;
+			index++; 
+
+			
+		}
+	}
+	cout<<New.height<<' '<<New.width<<'\n';
+	New.convertgraphtoimage();
+	return New;
+	
+}
+// bool checkgraphokay(graph &g)
+// {
+// 	int rowstart = g.topleft;
+// 	while(rowstart!=-1)
+// 	{
+// 		int colstart = rowstart;
+// 		while(colstart!=-1)
+// 		{
+// 			int right = colstart.right;
+
+// 		}
+// 	}
+// }
+graph insert_v_seam(graph &g)
+
+{
+	int m=g.width,n=g.height,i,j;
+
+	int cur = g.topleft, rowstart = g.topleft;
+	
+	for(i=0;i<n;i++)
+	{
+		cur = rowstart;
+		for(j=0;j<m;j++)
+		{
+			pixel_energy[i][j] = g.getEnergy(cur);
+			cur = g.pixelarray[cur].right;
+		}
+		rowstart = g.pixelarray[rowstart].bottom;
+	}
+	for(i=0;i<m;i++)
+		inner_dp[0][i] = pixel_energy[0][i];
+	for(i=1;i<n;i++)
+	{
+		for(j=0;j<m;j++)
+		{
+			double e = pixel_energy[i][j];
+			inner_dp[i][j] = e + inner_dp[i-1][j];
+			inner_choice[i][j] = j;
+			if(j && inner_dp[i][j] > e + inner_dp[i-1][j-1])
+			{
+				inner_dp[i][j] = e + inner_dp[i-1][j-1];
+				inner_choice[i][j] = j-1;
+			}
+			if(j+1 < m && inner_dp[i][j] > e + inner_dp[i-1][j+1])
+			{
+				inner_dp[i][j] = e + inner_dp[i-1][j+1];
+				inner_choice[i][j] = j+1;
+			}
+		}
+	}	
+	double minn = 1e50;
+	int minpos;
+	for(i=0;i<m;i++)
+	{
+		if(inner_dp[n-1][i] < minn)
+		{
+			minn = inner_dp[n-1][i];
+			minpos = i;
+		}
+	}
+
+	cur = minpos;
+	pos[n-1] = minpos;
+	for(i=n-2;i>=0;--i)
+	{
+		cur = inner_choice[i+1][cur];
+		pos[i] = cur;
+	}
+
+	graph New;
+	New = g;
+	New.width = New.width + 1;
+	cur = getptr(New,n-1,pos[n-1]);
+	int Left = New.pixelarray[cur].left, Right = New.pixelarray[cur].right;
+		//create new pixel at index 
+		int index = New.pixelarray_size;
+		New.pixelarray.push_back(pixel(New.pixelarray[cur].val[0],New.pixelarray[cur].val[1],New.pixelarray[cur].val[2] ));
+		New.pixelarray_size ++;
+
+	for(i=n-1;i>=0;--i)
+	{
+		
+			cout<<"here1"<<endl;
+			New.pixelarray[cur].right = index;
+			New.pixelarray[Left].updated = false;
+		
+		if(Right!=-1)
+		{
+			New.pixelarray[Right].left = index;
+			New.pixelarray[Right].updated = false;
+		}
+		New.pixelarray[index].left = Left;
+		New.pixelarray[index].right = Right;
+
+		if(!i)
+		{
+			if(cur == New.topleft)
+				New.topleft = New.pixelarray[cur].right;
+			break;
+		}
+		if(pos[i]==pos[i-1]){
+			cout<<"here2"<<endl;
+
+			cur = New.pixelarray[cur].top;
+			Left = New.pixelarray[cur].left; Right = New.pixelarray[cur].right;
+		//create new pixel at index 
+		int next_index = New.pixelarray_size;
+							New.pixelarray.push_back(pixel(New.pixelarray[cur].val[0],New.pixelarray[cur].val[1],New.pixelarray[cur].val[2]));
+
+		New.pixelarray_size ++;
+
+		New.pixelarray[next_index].bottom = index;
+		New.pixelarray[index].top = next_index;
+		index = next_index;
+		}
+		else if(pos[i]>pos[i-1])
+		{
+
+						cout<<"here3"<<endl;
+
+			int nexttop = New.pixelarray[cur].top;
+
+			cur = New.pixelarray[New.pixelarray[cur].top].left;
+			Left = New.pixelarray[cur].left; Right = New.pixelarray[cur].right ;
+			New.pixelarray.push_back(pixel(New.pixelarray[cur].val[0],New.pixelarray[cur].val[1],New.pixelarray[cur].val[2]));
+
+			// int next_index = New.pixelarray_size;
+		New.pixelarray_size ++;
+
+
+
+			New.pixelarray[index].top = nexttop;
+			New.pixelarray[Left].updated = false;
+
+			New.pixelarray[nexttop].bottom = index;
+			New.pixelarray[Right].updated = false;
+			index++;
+
+		}
+		else
+		{
+						cout<<"here4"<<endl;
+
+			int nexttop = New.pixelarray[New.pixelarray[cur].top].right;
+
+			cur = New.pixelarray[New.pixelarray[cur].top].right;
+			Left = New.pixelarray[cur].left; Right = New.pixelarray[cur].right;
+			New.pixelarray.push_back(pixel(New.pixelarray[cur].val[0],New.pixelarray[cur].val[1],New.pixelarray[cur].val[2]));
+
+			// int next_index = New.pixelarray_size;
+		New.pixelarray_size ++;
+
+
+
+			New.pixelarray[index].top = nexttop;
+			New.pixelarray[Left].updated = false;
+			if(nexttop!=-1)
+			{
+				New.pixelarray[nexttop].bottom = index;
+				New.pixelarray[Right].updated = false;
+			}
+			index++;
+
+			// int Right = New.pixelarray[cur].right, Left = New.pixelarray[cur].top;
+			// New.pixelarray[Right].top = Left;
+			// New.pixelarray[Right].updated = false;
+			
+			// New.pixelarray[Left].bottom = Right;
+			// New.pixelarray[Left].updated = false;
+			
+			// cur = New.pixelarray[New.pixelarray[cur].top].right;	
+		}
+	}
+	cout<<New.height<<' '<<New.width<<'\n';
+	// checkgraphokay(New);
+	// New.convertgraphtoimage();
+
+	return New;
+	
+
+}
+void upscale(const Mat &image, int r,int c)
+{
+
+	cout<<"upscaling!\n";
+	graph g(image);
+	if(r<=c)
+	{
+		if(r>0)
+		{
+			double q = ((double)c)/r;
+			double remaining = q;
+			while(r--)
+			{
+				while(remaining > 0.5)
+				{
+					c--;
+					remaining--;
+					g = insert_v_seam(g);
+				}
+				g = insert_h_seam(g);
+				remaining += q;
+			}
+		}
+		for(int i=0;i<c;i++)
+			g = insert_v_seam(g);
+	}
+	else
+	{
+		if(c>0)
+		{
+			double q = ((double)r)/c;
+			double remaining = q;
+			while(c--)
+			{
+				while(remaining > 0.5)
+				{
+					r--;
+					remaining--;
+					g = insert_h_seam(g);
+				}
+				g = insert_v_seam(g);
+				remaining += q;
+			}
+		}
+		for(int i=0;i<r;i++)
+			g = insert_h_seam(g);
+	}
+	g.convertgraphtoimage();
+}
+//r_height == final height in terms of original height. ex (1.25)
+void upscale(const Mat &image, double r_height, double r_width)
+{
+	upscale(image, (int)(image.size().height*(r_height - 1)), (int)(image.size().width*(r_width -1)));
+}
 int main()
 {
 	// cout<<"starting!"<<endl;
-	Mat image = imread("scenery.jpg", CV_LOAD_IMAGE_COLOR);
+	Mat image = imread("../TestImages/group2.jpg", CV_LOAD_IMAGE_COLOR);
 	if(! image.data )							  // Check for invalid input
 	{
 		cout <<  "Could not open or find the image" << std::endl ;
 		return -1;
 	}
-	// namedWindow( "original image window!!!", WINDOW_AUTOSIZE );
-	// imshow( "original image window!!!", image ); 
+	 namedWindow( "original image window!!!", WINDOW_AUTOSIZE );
+	 imshow( "original image window!!!", image ); 
 	// waitKey(0);  
 	graph g(image);
-	downscale(image, 0.5, 1.0);
+upscale(image, 1.0, 1.2);					g.convertgraphtoimage();;
 	return 0;
 
 }
