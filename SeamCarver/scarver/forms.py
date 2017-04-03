ada0fcb8298323eefc60b26cf8ade24277d82898
Sharan-Lobana <sps27iitr@gmail.com>
2017-04-04 02:35:48 +0530

    Images getting displayed successfully

--- SeamCarver/scarver/forms.py
+++ SeamCarver/scarver/forms.py
@@ -2,5 +2,7 @@ from django import forms
 
 class ImageUploadForm(forms.Form):
     image = forms.ImageField()
+
+class ImageResizeForm(forms.Form):
     desired_width = forms.IntegerField()    #in pixels
     desired_height = forms.IntegerField()   #in pixels
--- SeamCarver/scarver/views.py
+++ SeamCarver/scarver/views.py
@@ -1,14 +1,14 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from .forms import ImageUploadForm
+from .forms import ImageUploadForm,ImageResizeForm
 import os
 from django.core.files.images import get_image_dimensions
 
 # Create your views here.
 
 def saveImage(f):
-    root_path = os.path.dirname(os.path.dirname(os.path.realpath(__file__)))
-    save_path = root_path+os.sep+'UploadedImages'+os.sep+f.name
+    app_path = os.path.dirname(os.path.realpath(__file__))
+    save_path = app_path+os.sep+'static'+os.sep+'UploadedImages'+os.sep+f.name
     with open(save_path, 'wb+') as destination:
         for chunk in f.chunks():
             destination.write(chunk)
@@ -18,6 +18,39 @@ def index(request):
     return HttpResponse("The app is running successfully.")
 
 def uploadImage(request):
+    if request.method == 'POST':
+        f = request.FILES.get('image',None)
+        w,h = get_image_dimensions(f)
+        print "=============="
+        print "Width is: "+str(w)
+        print "=============="
+        print "Height is: "+str(h)
+        print "=============="
+        print f.size
+        print "=============="
+        print f.content_type
+        print "=============="
+        print f.content_type_extra
+        print "=============="
+        if f:
+            save_path = saveImage(f)
+            print save_path
+            context = {
+                'resizeform':ImageResizeForm(),
+                'image_width':w,
+                'image_height':h,
+                'image_name':f.name,
+            }
+            return render(request,'scarver/displayImage.html',context)
+        else:
+            # TODO: write the error message of no file upload
+            return render(request,'scarver/uploadImage.html',{'form':ImageUploadForm()})
+    if request.method == 'GET':
+        print "Request is get"
+        form = ImageUploadForm()
+        return render(request,'scarver/uploadImage.html',{'form':form})
+
+def resizeImage(request):
     if request.method == 'POST':
         f = request.FILES.get('image',None)
         w,h = get_image_dimensions(f)
