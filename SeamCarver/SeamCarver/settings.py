b0e1129786668c171905325a9d0f855aec276bc6
Sharan-Lobana <sps27iitr@gmail.com>
2017-04-29 23:45:24 +0530

    Improved UI

--- SeamCarver/SeamCarver/settings.py
+++ SeamCarver/SeamCarver/settings.py
@@ -38,7 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'scarver',
-    'crispy_forms',
+    
 ]
 
 MIDDLEWARE = [
--- SeamCarver/scarver/forms.py
+++ SeamCarver/scarver/forms.py
@@ -1,7 +1,39 @@
 from django import forms
+from django.utils.safestring import mark_safe
+from django.utils.html import escape, conditional_escape
+from django.utils.encoding import force_unicode
+from django.forms.widgets import ClearableFileInput, Input, CheckboxInput
+
+class CustomClearableFileInput(ClearableFileInput):
+
+    def render(self, name, value, attrs=None):
+        substitutions = {
+            #uncomment to get 'Currently'
+            'initial_text': "", # self.initial_text,
+            'input_text': self.input_text,
+            'clear_template': '',
+            'clear_checkbox_label': self.clear_checkbox_label,
+            }
+        template = '%(input)s'
+        substitutions['input'] = Input.render(self, name, value, attrs)
+
+        if value and hasattr(value, "url"):
+            template = self.template_with_initial
+            substitutions['initial'] = ('<img src="%s" alt="%s"/>'
+                                        % (escape(value.url),
+                                           escape(force_unicode(value))))
+            if not self.is_required:
+                checkbox_name = self.clear_checkbox_name(name)
+                checkbox_id = self.clear_checkbox_id(checkbox_name)
+                substitutions['clear_checkbox_name'] = conditional_escape(checkbox_name)
+                substitutions['clear_checkbox_id'] = conditional_escape(checkbox_id)
+                substitutions['clear'] = CheckboxInput().render(checkbox_name, False, attrs={'id': checkbox_id})
+                substitutions['clear_template'] = self.template_with_clear % substitutions
+
+        return mark_safe(template % substitutions)
 
 class ImageUploadForm(forms.Form):
-    image = forms.ImageField()
+    image = forms.ImageField(widget=CustomClearableFileInput())
     # image.widget.attrs['class'] = 'form-control'
 
 
